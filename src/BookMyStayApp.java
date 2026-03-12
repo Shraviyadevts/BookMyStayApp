731632e0b37bcab716d4e35198621f3356239e64
Shraviyadevts <shraviyadevts@gmail.com>
2026-03-12 10:15:36 +0530

    UC2:BasicRoomTypesAndStaticAvailability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,38 +1,110 @@
 /**
  * ============================================================
- * MAIN CLASS - UseCase1HotelBookingApp
+ * MAIN CLASS - UseCase2RoomInitialization
  * ============================================================
  *
- * Use Case 1: Application Entry & Welcome Message
+ * Use Case 2: Basic Room Types & Static Availability
  *
  * Description:
- * This class represents the entry point of the
- * Hotel Booking Management System application.
- * It demonstrates how a Java program starts
- * execution using the main() method and prints
- * a welcome message to the console.
+ * Demonstrates object modeling using abstraction,
+ * inheritance, and polymorphism for hotel room types.
  *
- * The application displays the system name
- * and version information before terminating.
+ * Room types:
+ * - Single Room
+ * - Double Room
+ * - Suite Room
+ *
+ * Availability is stored using simple variables.
  *
  * @author Developer
- * @version 1.0
+ * @version 2.1
  */
 
 public class BookMyStayApp {
 
-    /**
-     * Application entry point.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
-        System.out.println("=====================================");
-        System.out.println(" Welcome to Book My Stay Application ");
-        System.out.println(" Hotel Booking Management System ");
-        System.out.println(" Version : 1.0 ");
-        System.out.println("=====================================");
+        // Create room objects
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
 
+        // Static availability variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("===== Book My Stay - Room Availability =====\n");
+
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleAvailable);
+        System.out.println();
+
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteAvailable);
+    }
+}
+
+
+/**
+ * Abstract Room Class
+ * Defines common properties of all room types.
+ */
+abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Size      : " + size + " sq ft");
+        System.out.println("Price     : $" + price);
+    }
+}
+
+
+/**
+ * Single Room Class
+ */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 100.0);
+    }
+}
+
+
+/**
+ * Double Room Class
+ */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 180.0);
+    }
+}
+
+
+/**
+ * Suite Room Class
+ */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 500, 300.0);
     }
 }
